ec18a95340f1e98b8a71baf1637b3dafa3d832ae
Nimit Trevadiya <68644815+nimit0703@users.noreply.github.com>
2022-02-23 20:33:53 +0530

    Update script.js

--- notes/js/script.js
+++ notes/js/script.js
@@ -77,14 +77,8 @@ search.addEventListener("input", function(){
         else{
             element.style.display = "none";
         }
-        // console.log(cardTxt);
+     
     })
 })
 
-/*
-Further Features:
-1. Add Title
-2. Mark a note as Important
-3. Separate notes by user
-4. Sync and host to web server 
-*/ 
+
