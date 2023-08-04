04010b86d8e3c5262f602941d4ccceb5e769cbe4
nimit0703 <nimit.190410107143@gmail.com>
2023-08-04 08:51:44 +0530

    update in script.js

--- product-search-engine/script.js
+++ product-search-engine/script.js
@@ -1,3 +1,6 @@
 const search = ()=>{
-
+    const searchbox = document.getElementById("search-item").ariaValueMax.toUpperCase();
+    const storeitems = document.getElementById("product-list")
+    const product = document.querySelectorAll(".product")
+    const productname = document.getElementsByTagName("h2")
 };
