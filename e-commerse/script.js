0bc0e717a7465d61b7584a95290a871954e1eb5c
nimit0703 <nimit.190410107143@gmail.com>
2023-09-23 20:44:32 +0530

    home ,product page phase 1

--- e-commerse/script.js
+++ e-commerse/script.js
@@ -5,18 +5,16 @@ const cartPage = document.getElementById("cart-section");
 
 // Function to show a specific page and hide others
 function showPage(pageToShow) {
+    pageToShow = document.getElementById(pageToShow);
     const pages = [homePage, productPage, cartPage];
     pages.forEach((page) => {
-        page.style.display = page === pageToShow ? "block" : "none";
+        page.style.display = (page == pageToShow ? "block" : "none");
     });
 }
 
 
 // Add event listeners to navigation links
-const homeLink = document.getElementById("home-link");
-const productLink = document.getElementById("product-link");
-const cartLink = document.getElementById("cart-link");
-
-homeLink.addEventListener("click", () => showPage(homePage));
-productLink.addEventListener("click", () => showPage(productPage));
-cartLink.addEventListener("click", () => showPage(cartPage));
+document.addEventListener("DOMContentLoaded", function () {
+    // Your code here
+    showPage('home-section');
+});
