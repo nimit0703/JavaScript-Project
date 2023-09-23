dcdd59c4e3ea5d013c582e17ef0d8aae3dc679e9
nimit0703 <nimit.190410107143@gmail.com>
2023-09-23 19:31:40 +0530

    started from scratch

--- e-commerse/script.js
+++ e-commerse/script.js
@@ -1,7 +1,7 @@
 // Get references to the pages
-const homePage = document.getElementById("home-page");
-const productPage = document.getElementById("product-page");
-const cartPage = document.getElementById("cart-page");
+const homePage = document.getElementById("home-section");
+const productPage = document.getElementById("product-section");
+const cartPage = document.getElementById("cart-section");
 
 // Function to show a specific page and hide others
 function showPage(pageToShow) {
@@ -11,6 +11,7 @@ function showPage(pageToShow) {
     });
 }
 
+
 // Add event listeners to navigation links
 const homeLink = document.getElementById("home-link");
 const productLink = document.getElementById("product-link");
@@ -19,40 +20,3 @@ const cartLink = document.getElementById("cart-link");
 homeLink.addEventListener("click", () => showPage(homePage));
 productLink.addEventListener("click", () => showPage(productPage));
 cartLink.addEventListener("click", () => showPage(cartPage));
-
-
-
-
-
-
-class Product {
-    constructor(productName, ProductDesc, ProductPrice, imgSrc) {
-        this.name = productName;
-        this.desc = ProductDesc;
-        this.price = ProductPrice;
-        this.img = imgSrc;
-    }
-}
-
-
-
-document.querySelectorAll(".view-product").forEach(function(button){
-    button.addEventListener('click',function(event){
-        let productSelected = event.target.closest('.my-card');
-        
-        let imgSrc = productSelected.querySelector('img').src;
-        let productName = productSelected.getElementsByClassName('product-name')[0].innerHTML;
-        let ProductDesc = productSelected.getElementsByClassName('product-des')[0].innerHTML;
-        let ProductPrice = productSelected.getElementsByClassName('product-price')[0].innerHTML;
-        let productData = new Product(productName, ProductDesc, ProductPrice, imgSrc);
-        console.log(productData);
-        const queryString = `?productData=${encodeURIComponent(JSON.stringify(productData))}`;
-        // console.log(queryString);
-        console.log(`product.html${queryString}`);
-
-        window.location.href = `product.html${queryString}`;
-
-    })
-})
-document.getElementsByClassName
-console.log(typeof(document.querySelectorAll('.view-product').forEach))
