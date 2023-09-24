15a95914d40e275cbb981c274d8a6c308345b2b5
nimit0703 <nimit.190410107143@gmail.com>
2023-09-24 19:36:58 +0530

    added basic Lodash functions

--- e-commerse/lodashScript.js
+++ e-commerse/lodashScript.js
@@ -5,7 +5,7 @@ const cartPage = document.getElementById("cart-section");
 function showPage(pageToShow) {
   pageToShow = document.getElementById(pageToShow);
   const pages = [homePage, productPage, cartPage];
-  pages.forEach((page) => {
+  _.forEach(pages, (page) => {
     page.style.display = page == pageToShow ? "block" : "none";
   });
 }
@@ -20,23 +20,20 @@ document.querySelectorAll(".view-product").forEach(function (button) {
   button.addEventListener("click", function (event) {
     const productCard = event.target.closest(".card");
 
-    const productId = productCard.getAttribute("data-product-id");
-    const productName =
-      productCard.getElementsByClassName("prod-name")[0].innerHTML;
-    const productDesc =
-      productCard.getElementsByClassName("prod-desc")[0].innerHTML;
-    const productAmount =
-      productCard.getElementsByClassName("prod-amount")[0].innerHTML;
-    const productImg = productCard.getElementsByClassName("prod-img")[0].src;
+    const productId = _.get(productCard, 'dataset.productId');
+    const productName = _.get(productCard.querySelector('.prod-name'), 'textContent');
+    const productDesc = _.get(productCard.querySelector('.prod-desc'), 'textContent');
+    const productAmount = _.get(productCard.querySelector('.prod-amount'), 'textContent');
+    const productImg = _.get(productCard.querySelector('.prod-img'), 'src');
 
     console.log(productName);
-    let productSelected = new Product(productId,productName,productDesc,productAmount,productImg
-    );
+    let productSelected = new Product(productId, productName, productDesc, productAmount, productImg);
     setProductPagePara(productSelected);
   });
 });
+
 class Product {
-  constructor(productId, productName, productDesc,amount, productImg) {
+  constructor(productId, productName, productDesc, amount, productImg) {
     this.id = productId;
     this.name = productName;
     this.desc = productDesc;
@@ -46,38 +43,36 @@ class Product {
 }
 
 function setProductPagePara(productDetails) {
-//   console.log(productDetails);
   const productSection = document.getElementById("product-section");
   const img = productSection.querySelector("img");
   img.src = productDetails.imgSrc;
-  const name = productSection.querySelector("#product-name"); // Use querySelector with #
+  const name = productSection.querySelector("#product-name");
   name.innerHTML = productDetails.name;
-  const desc = productSection.querySelector("#product-desc"); // Use querySelector with #
+  const desc = productSection.querySelector("#product-desc");
   desc.innerHTML = productDetails.desc;
-  const amount = productSection.querySelector("#product-amount"); // Use querySelector with #
+  const amount = productSection.querySelector("#product-amount");
   amount.innerHTML = productDetails.amount;
-
 
   const addToCartButton = productSection.querySelector(".btn.btn-warning");
 
-  addToCartButton.addEventListener('click',function(){
+  addToCartButton.addEventListener('click', function () {
     cartItems.push(productDetails);
     addToCartDOM(productDetails);
     refreshBill();
     document.getElementById("cart-change").style.display = 'block';
     document.getElementById("cart-default").style.display = 'none';
-  
-  })
+  });
 }
-function refreshBill(){
-  const amount = cartItems.reduce((accumulator, currentItem) => {
+
+function refreshBill() {
+  const amount = _.reduce(cartItems, (accumulator, currentItem) => {
     return accumulator + parseFloat(currentItem.amount);
   }, 0);
-  
+
   const tax = amount * 0.25;
-  const dCharge = (amount+tax) >= 5000 ? 0:150;
-  const totalAmount  = amount+ tax + dCharge;
-  console.log(amount,tax,dCharge, totalAmount);
+  const dCharge = (amount + tax) >= 5000 ? 0 : 150;
+  const totalAmount = amount + tax + dCharge;
+  console.log(amount, tax, dCharge, totalAmount);
 
   document.getElementById("amount").innerHTML = amount;
   document.getElementById("tax").innerHTML = tax;
@@ -85,7 +80,7 @@ function refreshBill(){
   document.getElementById("total-amount").innerHTML = totalAmount;
 }
 
-function addToCartDOM(productDetails){
+function addToCartDOM(productDetails) {
   const cartProducts = document.getElementById('cart-products');
   const newCartProduct = createCartProduct(productDetails);
   cartProducts.appendChild(newCartProduct);
@@ -127,11 +122,11 @@ function createCartProduct(productDetails) {
   return cartProduct;
 }
 
-function paymentDone(){
+function paymentDone() {
   cartItems.length = 0;
   document.getElementById('cart-products').innerHTML = '';
   document.getElementById("cart-change").style.display = 'none';
   document.getElementById("cart-default").style.display = 'block';
 }
-const cartItems = [];
 
+const cartItems = [];
