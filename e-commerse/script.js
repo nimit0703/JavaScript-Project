2e2d8e7f126ba5cf5839b1cafc430f21e4369787
nimit0703 <nimit.190410107143@gmail.com>
2023-09-24 11:21:46 +0530

    add to cart done properly

--- e-commerse/script.js
+++ e-commerse/script.js
@@ -5,33 +5,67 @@ const cartPage = document.getElementById("cart-section");
 
 // Function to show a specific page and hide others
 function showPage(pageToShow) {
-    pageToShow = document.getElementById(pageToShow);
-    const pages = [homePage, productPage, cartPage];
-    pages.forEach((page) => {
-        page.style.display = (page == pageToShow ? "block" : "none");
-    });
+  pageToShow = document.getElementById(pageToShow);
+  const pages = [homePage, productPage, cartPage];
+  pages.forEach((page) => {
+    page.style.display = page == pageToShow ? "block" : "none";
+  });
 }
-
 
 // Add event listeners to navigation links
 document.addEventListener("DOMContentLoaded", function () {
-    // Your code here
-    showPage('home-section');
+  // Your code here
+  showPage("home-section");
 });
 
+document.querySelectorAll(".view-product").forEach(function (button) {
+  button.addEventListener("click", function (event) {
+    const productCard = event.target.closest(".card");
 
-document.querySelectorAll('.view-product').forEach(function (button){
-    button.addEventListener('click',function(event){
-        const productCard = event.target.closest(".card");
+    // Retrieve data from the clicked card
+    const productId = productCard.getAttribute("data-product-id");
+    const productName =
+      productCard.getElementsByClassName("prod-name")[0].innerHTML;
+    const productDesc =
+      productCard.getElementsByClassName("prod-desc")[0].innerHTML;
+    const productImg = productCard.getElementsByClassName("prod-img")[0].src;
 
-        // Retrieve data from the clicked card
-        const productId = productCard.getAttribute("data-product-id");
-        const productName = productCard.getElementsByClassName('prod-name').textContent;
-        const productDesc = productCard.getElementsByClassName('prod-desc').textContent;
-        const productImg = productCard.getElementsByClassName('prod-img').src;
-
-        console.log()
-    })
+    console.log(productName);
+    let productSelected = new Product(
+      productId,
+      productName,
+      productDesc,
+      productImg
+    );
+    setProductPagePara(productSelected);
+  });
 });
+class Product {
+  constructor(productId, productName, productDesc, productImg) {
+    this.id = productId;
+    this.name = productName;
+    this.desc = productDesc;
+    this.imgSrc = productImg;
+  }
+}
 
-document.getElementsByClassName
+function setProductPagePara(productDetails) {
+//   console.log(productDetails);
+  const productSection = document.getElementById("product-section");
+  const img = productSection.querySelector("img");
+  img.src = productDetails.imgSrc;
+  const name = productSection.querySelector("#product-name"); // Use querySelector with #
+  name.innerHTML = productDetails.name;
+  const desc = productSection.querySelector("#product-desc"); // Use querySelector with #
+  desc.innerHTML = productDetails.desc;
+
+
+  const addToCartButton = productSection.querySelector(".btn.btn-warning");
+
+  addToCartButton.addEventListener('click',function(){
+    cartItems.push(productDetails);
+  })
+}
+
+
+const cartItems = [];
